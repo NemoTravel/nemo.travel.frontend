c013b2ea0f5e30726458bfe44abebef13395e7d1
Andrew <atomic.alienz@gmail.com>
2015-05-19 17:36:09 +0300

    0000 results WIP

--- js/vm/Flights/SearchResults/Group.js
+++ js/vm/Flights/SearchResults/Group.js
@@ -2,8 +2,10 @@
 define(
 	['knockout', 'js/vm/helpers', 'js/vm/BaseStaticModel'],
 	function (ko, helpers, BaseModel) {
-		function FlightsSearchResultsGroup (initialData) {
+		function Group (initialData) {
 			BaseModel.apply(this, arguments);
+
+			var self = this;
 
 			this.flightsById = {};
 			this.selectedFlight = ko.observable(this.flights[0]);
@@ -16,10 +18,19 @@ define(
 			for (var siter = 0; siter < this.flights[0].legs.length; siter++) {
 				var addObj = {
 						options: [],
-						selected: ko.observable()
+						selected: ko.observable(),
+						setOptionDisable: function (option, item) {
+							ko.applyBindingsToNode(
+								option,
+								{
+									disable: item.disabled
+								},
+								item
+							);
+						}
 					},
-					tmp = {},
 					tmpIter,
+					tmp = {},
 					key;
 
 				for (var fiter = 0; fiter < this.flights.length; fiter++) {
@@ -39,16 +50,73 @@ define(
 				// Converting to array
 				tmpIter = Object.keys(tmp);
 				for (var i = 0; i < tmpIter.length; i++) {
-					addObj.options.push(tmp[tmpIter[i]]);
+					var adder = {
+						flights: tmp[tmpIter[i]]
+					};
+
+					// Computed that decides wheter option is disabled
+					adder.disabled = ko.computed(function () {
+						var option = this.flights,
+							ret = false;
+
+						for (var i = 0; i < option.length; i++) {
+							ret = ret || !self.flightsById[option[i]].filteredOut();
+						}
+
+						return !ret;
+					}, adder);
+
+					addObj.options.push(
+//						tmp[tmpIter[i]]
+						adder
+					);
 				}
 
 				// Add sorting
+				addObj.options.sort(function (a, b) {
+					var af = self.flightsById[a.flights[0]],
+						bf = self.flightsById[b.flights[0]];
+
+					if (af.legs[siter].depDateTime.getTimestamp() != bf.legs[siter].depDateTime.getTimestamp()) {
+						return af.legs[siter].depDateTime.getTimestamp() - bf.legs[siter].depDateTime.getTimestamp();
+					}
+					else {
+						return af.legs[siter].arrDateTime.getTimestamp() - bf.legs[siter].arrDateTime.getTimestamp();
+					}
+				});
+
+				addObj.selected(addObj.options[0]);
+
+
 
 				this.legGroupings.push(addObj);
 			}
 
-			// TODO Defining set flight
-			console.log(this.legGroupings);
+			this.selectedFlights = ko.computed(function () {
+				var ret;
+
+				for (var i = 0; i < this.legGroupings.length; i++) {
+					var tmp = [],
+						flights = this.legGroupings[i].selected().flights,
+						i;
+
+					for (i = 0; i < flights.length; i++) {
+						if (
+							(
+								typeof ret == 'undefined' ||
+								ret.indexOf(flights[i]) >= 0
+							) &&
+							!this.flightsById[flights[i]].filteredOut()
+						) {
+							tmp.push(flights[i]);
+						}
+					}
+
+					ret = tmp;
+				}
+
+				return ret;
+			}, this);
 
 			this.filteredOut = ko.computed(function () {
 				for (var i = 0; i < this.flights.length; i++) {
@@ -75,21 +143,27 @@ define(
 			this.recommendRating = ko.computed(function () {
 				return this.selectedFlight().recommendRating;
 			}, this);
-
-
 		}
 
 		// Extending from dictionaryModel
-		helpers.extendModel(FlightsSearchResultsGroup, [BaseModel]);
+		helpers.extendModel(Group, [BaseModel]);
 
-		FlightsSearchResultsGroup.prototype.getTotalPrice = function () {
+		Group.prototype.getSelectedFlightsData = function (flarr, leg) {
+			var ret = '';
+
+			ret += this.flightsById[flarr.flights[0]].legs[leg].depDateTime.getISODateTime() + ' - ' + this.flightsById[flarr.flights[0]].legs[leg].arrDateTime.getISODateTime()
+
+			return ret;
+		};
+
+		Group.prototype.getTotalPrice = function () {
 			return this.flights[0].price.totalPrice;
 		};
 
-		FlightsSearchResultsGroup.prototype.getValidatingCompany = function () {
+		Group.prototype.getValidatingCompany = function () {
 			return this.flights[0].price.validatingCompany;
 		};
 
-		return FlightsSearchResultsGroup;
+		return Group;
 	}
 );
