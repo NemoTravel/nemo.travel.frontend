40220a7a9d1c8da7b01a16883416d47eefff1dfb
pnaumov <ecrofeg1@gmail.com>
2017-08-02 14:21:50 +0300

    update: 02.08.2017#1

--- js/vm/Flights/SearchResults/FareFeatures/Controller.js
+++ js/vm/Flights/SearchResults/FareFeatures/Controller.js
@@ -5,14 +5,36 @@ define(
 		function FlightsSearchResultsFareFeaturesController(componentParameters) {
 			BaseControllerModel.apply(this, arguments);
 			
-			var self = this;
-			
 			this.flight = componentParameters.flight;	
 			this.resultsController = componentParameters.resultsController;
 			this.isFullMode = componentParameters.fullMode === true;
+			this.isListMode = componentParameters.listMode === true;
 			this.leftColumn = [];
 			this.rightColumn = [];
 			this.miscColumn = [];
+
+			this.featuresAreVisible = ko.observable(false);
+			
+			this.icons = ko.pureComputed(function () {
+				var iconsList = [
+					{ name: 'baggage',      icon: 'fa fa-suitcase' },
+					{ name: 'exchangeable', icon: 'fa fa-refresh' },
+					{ name: 'vip_service',  icon: 'fa fa-star' },
+					{ name: 'refundable',   icon: 'fa fa-undo' }
+				];
+
+				return iconsList.map(function (item) {
+					var featureInfo = this.flight.fareFeatures.isFeatureAvailable(item.name);
+
+					return {
+						icon: item.icon,
+						isAvailable: featureInfo.isAvailable,
+						isVisible: featureInfo.isVisible,
+						title: this.$$controller.i18n('FlightsSearchResults', 'flightsGroup__fareFamilies__param__' + item.name),
+						description: featureInfo.description
+					};
+				}, this);
+			}, this);
 
 			if ('segmentId' in componentParameters && this.flight.fareFeatures.bySegments.hasOwnProperty(componentParameters.segmentId)) {
 				this.features = this.flight.fareFeatures.bySegments[componentParameters.segmentId];
@@ -21,26 +43,45 @@ define(
 				this.features = this.flight.fareFeatures.getFirstFamily();
 			}
 			
+			this.groupFeaturesByColumns();
+		}
+
+		helpers.extendModel(FlightsSearchResultsFareFeaturesController, [BaseControllerModel]);
+
+		/**
+		 * Прячет\показывает полный список фич на поисковой выдаче.
+		 */
+		FlightsSearchResultsFareFeaturesController.prototype.toggleFeaturesBlock = function () {
+			this.featuresAreVisible(!this.featuresAreVisible());
+		};
+
+		/**
+		 * Распределяет фичи семейства по трём колонкам:
+		 * - багаж
+		 * - возврат + обмен
+		 * - всё остальное
+		 */
+		FlightsSearchResultsFareFeaturesController.prototype.groupFeaturesByColumns = function () {
 			if (this.features && this.features.hasFeatures) {
 				if (this.features.list.hasOwnProperty('baggage')) {
 					this.leftColumn = this.features.list.baggage;
 				}
-				
+
 				if (this.features.list.hasOwnProperty('refunds')) {
 					this.rightColumn = this.features.list.refunds;
 				}
-				
+
 				if (this.features.list.hasOwnProperty('misc')) {
 					this.miscColumn = this.features.list.misc;
 				}
-				
+
 				if (!this.isFullMode) {
 					var newLeftColumn = [],
 						newRightColumn = [];
 
 					newLeftColumn = newLeftColumn.concat(this.leftColumn);
 					newRightColumn = newRightColumn.concat(this.rightColumn);
-					
+
 					this.miscColumn.map(function (feature) {
 						if (feature.code === 'seats_registration') {
 							newRightColumn.push(feature);
@@ -56,9 +97,7 @@ define(
 
 				this.fareRulesLinkText = this.$$controller.i18n('FlightsSearchResults', 'flightsGroup__fareFamilies__title__tariff') + ' &laquo;' + this.features.name + '&raquo;';
 			}
-		}
-
-		helpers.extendModel(FlightsSearchResultsFareFeaturesController, [BaseControllerModel]);
+		};
 
 		return FlightsSearchResultsFareFeaturesController;
 	}
